351c53ef668f18a1b23f6d30932a3ef7067e3ea6
alisonmoldovan-mauer <moldovanmaueal101438@th-nuernberg.de>
2026-01-09 09:56:26 +0100

    readme

--- plots/election_2025.py
+++ plots/election_2025.py
@@ -1,12 +1,18 @@
 import pandas as pd
+import sys
+from pathlib import Path
+import os
+print(os.getcwd())
 
-df = pd.read_csv("data/processed/Niederbayern Wahlergebnisse.csv")
+#file_path = Path(__file__).parent / "processed" / "Niederbayern Wahlergebnisse.csv"
+#df = pd.read_csv(file_path)
+print(os.getcwd())
 
-df = df.drop(columns=["Aggregat", "Kennziffer"])
+df = pd.read_csv("/processed/Niederbayern Wahlergebnisse.csv")
 
-cols_to_conv = [col for col in df.columns if col != "Gemeinde"]
+df = df.drop(columns=["Aggregat", "Kennziffer", "Raumeinheit"])
 
-for c in cols_to_conv:
+for c in df.columns:
     df[c] = (df[c]
                 .str.replace(".", "", regex=False)
                 .str.replace(",", ".", regex=False)
